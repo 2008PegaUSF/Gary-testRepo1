b89b245d9763b401e20977c210481c09e4a159d7
unknown <keng4105@gmail.com>
2020-08-17 21:37:40 -0400

    initial

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		Scanner scan = new Scanner(System.in);
+		//(we created a scanner object)
+		String input = scan.next();
+		//(we creating a string to read the input,
+		//stating that the string is equal to the next input)
+		System.out.println(input);
+		//(printing the input)
+	}
+
+}
